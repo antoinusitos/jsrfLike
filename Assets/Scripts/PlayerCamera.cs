ee15eb8767ed4f173c7a4f73ec3cb1af509dfd83
antoinusitos <a.gargasson@rubika-edu.com>
2020-04-28 19:22:24 +0200

    first movements, first camera, doing jump

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField]
+    private float myHorizontalSpeed = 2.0f;
+
+    [SerializeField]
+    private float myVerticalSpeed = 2.0f;
+
+    private Transform myTransform = null;
+
+    private void Start()
+    {
+        myTransform = transform;
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("RS_X");
+
+        if(Mathf.Abs(x) > 0.1f)
+            myTransform.Rotate(Vector3.up, myHorizontalSpeed * x * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float mySpeedMax = 10.0f;
+    private float mySpeed = 0.0f;
+
+    [SerializeField]
+    private Transform myCamera = null;
+
+    [SerializeField]
+    private Rigidbody myRigidBody = null;
+
+    [SerializeField]
+    private float myAcceleration = 2.0f;
+
+    [SerializeField]
+    private float myDecceleration = 2.0f;
+
+    [SerializeField]
+    private float myDamp = 1.0f;
+
+    [SerializeField]
+    private Transform myBody = null;
+
+    private Vector3 myLastDir = Vector3.zero;
+
+    [SerializeField]
+    private float myJumpForce = 5;
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 camForward = myCamera.forward;
+        camForward.y = 0;
+        camForward *= z;
+
+        Vector3 camRight = myCamera.right;
+        camRight.y = 0;
+        camRight *= x;
+
+        Vector3 dir = camForward + camRight;
+
+        if (x != 0 || z != 0)
+        {
+            mySpeed += myAcceleration * Time.deltaTime;
+            if (mySpeed > mySpeedMax)
+                mySpeed = mySpeedMax;
+
+            myLastDir = dir;
+        }
+        else if(mySpeed > 0)
+        {
+            mySpeed -= myDecceleration * Time.deltaTime;
+            if (mySpeed < 0)
+                mySpeed = 0;
+            dir = myLastDir;
+        }
+
+        Vector3 velocity = Vector3.zero;
+        Vector3 damp = Vector3.SmoothDamp(myRigidBody.position, myRigidBody.position + dir.normalized * Time.fixedDeltaTime * mySpeed, ref velocity, myDamp);
+
+        myRigidBody.MovePosition(damp);
+
+        myBody.LookAt(myRigidBody.position + dir.normalized);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidBody.velocity += Vector3.up * myJumpForce;
+        }
+    }
+}
